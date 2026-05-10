cc34eaa598b72d1ab14b7a4d846f49a970ee9836
Joonhee Lee <2joonh2@gmail.com>
2026-05-10 19:22:33 +0900

    Revert "serverless release trial v1.3"
    This reverts commit 6e27e9e426ea6aa8f89aa531baa0c1d178599d68.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/vdgopher_for_sql/',
+  base: process.env.VITE_BASE_URL || '/',
   worker: {
     format: 'es',
   },
